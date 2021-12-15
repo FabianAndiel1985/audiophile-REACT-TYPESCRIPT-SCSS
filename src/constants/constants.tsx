926736e61bac0936487e412c1c75e484e906bdf6
Fabian Andiel <fabian.andiel@yahoo.com>
2021-12-15 09:43:00 +0100

    sets up basic constant files

---
+++ src/constants/constants.tsx
@@ -0,0 +1,7 @@
+export const black = "#000000"
+export const brightBlack = "#101010"
+export const silver= "#F1F1F1"
+export const brightSilver= "#FAFAFA"
+export const white = "#FFFFFF"
+export const brightOrange = "#fbaf85"
+export const darkOrange = "#D87D4A"
